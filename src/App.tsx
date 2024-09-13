0c4e7b78399540e3e71a9faff9d54b2042fe9770
akhdrzki <akhadirizky@gmail.com>
2024-09-13 09:23:57 +0800

    feat: init NavbarMobile, wrapped listParent and aims for Mobile breakpoints
    represented `organism`, reference to `Atomic Design`

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import React, { } from 'react'
+import { NavbarMobile } from './components/Fragments/NavbarMobile'
+import { NavbarDesktop } from './components/Fragments/NavbarDesktop'
 
-function App() {
+export const App = () => {
+
   return (
-    <>
-      <h1 className='text-2xl font-bold'>Hello world!!</h1>
-    </>
+    <div>
+      <NavbarMobile></NavbarMobile>
+      <NavbarDesktop></NavbarDesktop>
+    </div>
   )
 }
 
-export default App
