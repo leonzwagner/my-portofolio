fcbfa01dabc033a2ff1ca924556fa6679ba8b8a7
akhdrzki <akhadirizky@gmail.com>
2024-09-13 09:30:53 +0800

    style: remove component that declared in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React, { } from 'react'
-import { NavbarMobile } from './components/Fragments/NavbarMobile'
-import { NavbarDesktop } from './components/Fragments/NavbarDesktop'
 
 export const App = () => {
 
   return (
-    <div>
-      <NavbarMobile></NavbarMobile>
-      <NavbarDesktop></NavbarDesktop>
-    </div>
+    <div></div>
   )
 }
 
