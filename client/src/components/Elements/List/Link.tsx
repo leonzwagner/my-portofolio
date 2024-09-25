9f15900d382ea8d81d7e5b7f46b063a86f8718ec
akhdrzki <akhadirizky@gmail.com>
2024-09-25 08:33:38 +0800

    feat: new navbar style

--- client/src/components/Elements/List/Link.tsx
+++ client/src/components/Elements/List/Link.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 interface LinkProps {
   children: React.ReactNode
-  href: string 
+  href: string
+  color: string
+  onMouseOver: (event: React.MouseEvent<HTMLDivElement>) => void
+  onMouseOut: (event: React.MouseEvent<HTMLDivElement>) => void
 }
 
-export const Link = ({ href = '/home', children }: LinkProps) => {
+export const Link = ({ href = '/home', children, color = 'decoration-lavender', onMouseOver, onMouseOut }: LinkProps) => {
 
   return (
-    <a className='p-2' href={href}>{children}</a>
+    <a className={`p-2 hover:underline decoration-2 ${color}`} onMouseOver={onMouseOver} onMouseOut={onMouseOut} href={href}>{children}</a>
   )
 }
 
