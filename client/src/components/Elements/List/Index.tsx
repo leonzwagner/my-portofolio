fe55fd6251e6694c49974e647668ff77943b4cf2
akhdrzki <87889949+akhdrzki@users.noreply.github.com>
2024-09-25 08:37:52 +0800

    Remake navbar (#12)
    * feat: new navbar style
    
    * feat: new navbar style
    
    * feat: new navbar style
    
    * chore: nothing change

--- client/src/components/Elements/List/Index.tsx
+++ client/src/components/Elements/List/Index.tsx
@@ -6,7 +6,7 @@ interface ListNavProps {
 
 export const ListNav = ({ children }: ListNavProps) => {
   return (
-    <nav className='text-sm rounded-lg font-semibold bg-lavender sticky top-0 text-text-mocha h-10 w-60 flex items-center justify-evenly sm:text-lg sm:w-64 sm:h-12'>{children}</nav>
+    <nav className='text-text-mocha'>{children}</nav>
   )
 }
 
