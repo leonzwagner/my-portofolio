88f11d58a522d356e9673ca89e16c3164da9e834
akhdrzki <akhadirizky@gmail.com>
2024-09-26 08:37:34 +0800

    feat: init image profile

---
+++ client/src/components/Elements/Image/Index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ImageProf = () => {
+  return (
+    <div className='rounded-full flex justify-center items-center'>
+      <img className='rounded-full w-52 h-52' src="https://i.pinimg.com/564x/5e/7a/7c/5e7a7c2a9f4d606cb1f4744e95f7ba7e.jpg" alt="" />
+    </div>
+  )
+}
+
