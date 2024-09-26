cd76ad82ff451a885fbdc5e8342cdc661cad710d
akhdrzki <87889949+akhdrzki@users.noreply.github.com>
2024-09-26 17:42:22 +0800

    Bio (#14)
    * refactor: adding new hover to link
    
    * refactor: add desktop breakpoint, and centered parent element
    
    * refactor: erase div parent
    
    * refactor: make it reusable by adding children in it
    
    * feat: init paragraph
    
    * refactor: erase div parent
    
    * feat: init bio `organism` part
    
    * refactor: write down all children props, as `organism`

--- client/src/components/Elements/Text/Index.tsx
+++ client/src/components/Elements/Text/Index.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { Heading } from './Heading'
 import { Span } from './Span'
 
-export const TextBio = () => {
+interface TextProps {
+  children: React.ReactNode
+  style: string
+}
+
+export const TextBio = ({ children, style }: TextProps) => {
   return (
-    <section className='flex flex-col space-y-5'>
-      <Heading style='text-3xl text-text-mocha'>Lalu Akhadi Rizki</Heading>
-      <Span style='text-2xl text-subtext1'>Self-Taught Software Engineer</Span>
-    </section>
+    <section className={style}>{children}</section>
   )
 }
 
+//<Heading style='text-3xl text-text-mocha'>Lalu Akhadi Rizki</Heading>
+//<Span style='text-2xl text-subtext1'>Self-Taught Software Engineer</Span>
+//<section className='flex flex-col space-y-5'>{children}</section>
--- client/src/components/Fragments/Profile.tsx
+++ client/src/components/Fragments/Profile.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { TextBio } from '../Elements/Text/Index'
 import { ImageProf } from '../Elements/Image/Index'
+import { Heading } from '../Elements/Text/Heading'
+import { Span } from '../Elements/Text/Span'
 
 export const Profile = () => {
   return (
     <div className='flex flex-col items-center space-y-5 sm:flex-row sm:justify-center my-10 sm:px-3 sm:my-16 sm:space-x-10'>
-      <TextBio />
+      <TextBio style='flex flex-col space-y-5 w-64'>
+        <Heading style='text-3xl text-text-mocha'>Lalu Akhadi Rizki</Heading>
+        <Span style='text-2xl text-subtext1'>Self-taught Software Engineer</Span>
+      </TextBio>
       <ImageProf />
     </div>
   )
